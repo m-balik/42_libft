9b7f5c7dff21fb386314d2edd1fda099a3f7c3c6
Muhammed Ikbal Akbas <muakbas@k2m08s06.42kocaeli.com.tr>
2025-06-10 16:17:43 +0300

    streln eklendi

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,25 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muakbas <muakbas@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/10 14:14:31 by muakbas           #+#    #+#             */
-/*   Updated: 2025/06/10 16:11:32 by muakbas          ###   ########.fr       */
+/*   Created: 2025/06/10 16:01:56 by muakbas           #+#    #+#             */
+/*   Updated: 2025/06/10 16:11:34 by muakbas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-#include <stdlib.h>
+size_t  ft_strlen(const char *s)
+{
+    size_t  i;
 
-
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
-
-size_t  ft_strlen(const char *s);
+    i = 0;
+    while (s[i])
+        i++;
+    return (i);
+}
